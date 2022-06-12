355ed54cdbd3713702602dca1b0dcc6440d057f0
Aman Jha <101969746+AmannJha@users.noreply.github.com>
2022-06-12 11:33:06 +0530

    Add files via upload

---
+++ 63_login_page.php
@@ -0,0 +1,12 @@
+<html>
+    <head><title>
+        FACEBOOK LOG-IN PAGE
+    </title></head>
+    <body>
+        <form>
+            <li> Email Id Or Mobile No.<br><input type="text" name="Email"> </li><br>
+            <li> Password <br><input type="Password" name="Password" ><br><a href=" ">Forget Password</a></li><br>
+            <li> <input type="Submit" name="log in" value="log in"></li><br><br>
+</form>
+</body>
+</html>
